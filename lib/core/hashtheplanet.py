e538a2b8f0bad1c07b4bb59948690a7d383c53e3
bretfourbe <gwendal@cyberwatch.fr>
2021-07-28 10:54:48 +0200

    Add repo structure and .gitignore

---
+++ lib/core/hashtheplanet.py
@@ -0,0 +1,33 @@
+"""
+The main module for HashThePlanet
+"""
+
+class HashThePlanet():
+    """
+    The HashThePlanet class
+    """
+
+    @staticmethod
+    def show_all_hashs():
+        """
+        Prints out all known hashs
+        """
+        # TODO: change code here
+        print("No hash to display")
+
+    @staticmethod
+    def compute_hashs():
+        """
+        Computes all hashs
+        """
+        # TODO: change code here
+        print("Computing done")
+
+def main():
+    """
+    The main function
+    """
+    print("#### HashThePlanet ####")
+    hashtheplanet = HashThePlanet()
+    hashtheplanet.compute_hashs()
+    hashtheplanet.show_all_hashs()
