7e6fcdfaf01874b53e46610856e81e8e57c4ed01
bretfourbe <gwendal@cyberwatch.fr>
2021-07-29 14:27:02 +0200

    Add loguru dependance for logging, add requirements.txt

--- lib/core/hashtheplanet.py
+++ lib/core/hashtheplanet.py
@@ -2,6 +2,15 @@
 The main module for HashThePlanet
 """
 
+import argparse
+import sys
+
+from loguru import logger
+
+logger.remove()
+
+HASHTHEPLANET_VERSION = "HashThePlanet 0.0.0"
+
 class HashThePlanet():
     """
     The HashThePlanet class
@@ -13,7 +22,7 @@ class HashThePlanet():
         Prints out all known hashs
         """
         # TODO: change code here
-        print("No hash to display")
+        logger.info("No hash to display")
 
     @staticmethod
     def compute_hashs():
@@ -21,13 +30,46 @@ class HashThePlanet():
         Computes all hashs
         """
         # TODO: change code here
-        print("Computing done")
+        logger.info("Computing done")
 
 def main():
     """
-    The main function
+    The main function, which is the entry point of the program.
+    It stores arguments provided by the user and launches hash computing.
     """
-    print("#### HashThePlanet ####")
+
+    parser = argparse.ArgumentParser(description="HashThePlanet-0.0.0")
+
+    parser.add_argument(
+        "--color",
+        action="store_true",
+        help="Colorize output"
+    )
+
+    parser.add_argument(
+        "-v", "--verbose",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING"],
+        help="Set verbosity level"
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        help="Show program's version number and exit",
+        version=HASHTHEPLANET_VERSION
+    )
+
+    args = parser.parse_args()
+
+    if args.color:
+        logger.add(sys.stdout, format="<lvl>{message}</lvl>", level=args.verbose)
+    else:
+        logger.add(sys.stdout, colorize=False, format="{message}", level=args.verbose)
+
+    logger.info("#### HashThePlanet ####")
     hashtheplanet = HashThePlanet()
+    logger.debug("Start computing hashs")
     hashtheplanet.compute_hashs()
+    logger.debug("Retieving computed hashs")
     hashtheplanet.show_all_hashs()
