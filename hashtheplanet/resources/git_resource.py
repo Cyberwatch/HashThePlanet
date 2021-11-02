a607d999bc8005d29c6cc4b1cc0adc8ae427c4ad
Julien <julien.tauran@orange.fr>
2021-11-02 11:06:27 +0100

    Improved git resource performance

--- hashtheplanet/resources/git_resource.py
+++ hashtheplanet/resources/git_resource.py
@@ -1,20 +1,31 @@
 """
 This module handles Git resources to generate hashs.
 """
-#standard imports
-import glob
+# standard imports
 import hashlib
 import os
+import subprocess
 import tempfile
+from stat import S_ISDIR, S_ISREG
+from typing import List, Tuple
 
 # third party imports
-from git import Git, Repo, GitCommandError
+from git import GitCommandError, Repo
+from git.diff import Diff, DiffIndex
+from git.objects.commit import Commit
+from git.refs.tag import Tag
 from loguru import logger
 
 # project imports
 from sql.db_connector import DbConnector
 
-EXTENSION = ["txt", "css", "js"]
+# types
+FilePath = str
+BlobHash = str
+TagName = str
+FileHash = str
+GitFileMetadata = Tuple[FilePath, TagName, BlobHash]
+FileMetadata = Tuple[FilePath, TagName, FileHash]
 
 class GitResource():
     """
@@ -27,90 +38,176 @@ class GitResource():
         self._database = database
 
     @staticmethod
-    def clone_repository(technology, url, path):
+    def clone_repository(url, path) -> Repo:
         """
         This method tries to clone the repository from the provided url.
         """
         logger.debug(f"Cloning repository {url} ...")
-        Git(path).clone(url)
-        repository = Repo(f"{path}/{technology}")
-        return repository
+        return Repo.clone_from(url, path, bare=True)
 
     @staticmethod
-    def get_tags(repository: Repo):
-        """
-        This method returns the list of tags sorted by commit date.
-        """
-        return sorted(repository.tags, key=lambda tag: tag.commit.committed_datetime)
-
-    @staticmethod
-    def get_hash(file):
+    def get_hash(file_content) -> str or None:
         """
         This method computes the hash of the provided file and returns it.
         """
-        try:
-            with open(file, 'rb') as file_descriptor:
-                file_bytes = file_descriptor.read() # read entire file as bytes
-                readable_hash = hashlib.sha256(file_bytes).hexdigest()
-                return readable_hash
-        except OSError as error:
-            logger.error(f"Error with file {file} : {error}")
-        return None
+        if file_content is None:
+            return None
+        return hashlib.sha256(file_content).hexdigest()
 
-    def checkout_and_compute(self, session, directory, repository: Repo, tag):
+    @staticmethod
+    def get_all_files_from_commit(commit: Commit) -> List[Tuple[FilePath, BlobHash]]:
         """
-        This method checkout the provided tag of the repository,
-        stores files information in the database and computes the hashs.
+        This method retrieves all files with their blob hash in a commit.
         """
-        repository.git.checkout(f"tags/{tag}")
-        technology = directory.split('/')[-1]
+        file_list = []
 
-        for root, _, _ in os.walk(directory):
-            for ext in EXTENSION:
-                for file_name in glob.glob(f"{root}/**/*.{ext}", recursive = True):
-                    index = file_name.find(technology)
-                    path = file_name[index:].lstrip(technology)
+        for blob in commit.tree.traverse():
+            if S_ISDIR(blob.mode):
+                continue
+            file_list.append((blob.path, blob.hexsha))
+        return file_list
 
-                    logger.debug(f"Current tag : {tag}")
-
-                    self._database.insert_file(session, technology, path)
-
-                    path_to_file = f"{directory}{path}"
-                    logger.debug(f"Path to file : {path_to_file}")
-                    hash_value = self.get_hash(path_to_file)
-                    logger.debug(f"Hash value : {hash_value}")
-
-                    self._database.insert_or_update_hash(session, hash_value, technology, tag)
-
-    def clone_checkout_and_compute_hashs(self, session_scope, url):
+    def _hash_files(
+        self,
+        files: List[GitFileMetadata],
+        repo_dir_path: str
+    ) -> List[FileMetadata]:
         """
-        This method clones the repository from url, retrieves and stores tags in the database,
-        then checkout each tag, sotres files information in the database and computes the hashs.
+        This method calculates the SHA256 hashes of input files.
+        To do so, it reads first the content by using the file blob hash with the command `git cat-file -p [blob hash]`,
+        then it calculates the hash.
+        """
+        files_info: List[Tuple[FilePath, TagName, FilePath]] = []
+        current_dir = os.getcwd()
+        os.chdir(repo_dir_path)
+
+        for (file_path, tag_name, blob_hash) in files:
+            try:
+                # We need to use a subprocess and not the GitPython library
+                # because when we execute "git cat-file -p [blob]" with it, it always removes the \n from the last line.
+                # Because of that when we calculate the hash of a file, it may change if it has originally a \n or not.
+                # (https://github.com/gitpython-developers/GitPython/blob/main/git/cmd.py#L947)
+                file_content = subprocess.check_output(
+                    ['git', 'cat-file', '-p', blob_hash],
+                    shell=False,
+                )
+                file_hash = self.get_hash(file_content)
+                files_info.append((file_path, tag_name, file_hash))
+            except (ValueError, subprocess.CalledProcessError) as exception:
+                logger.error(exception)
+        os.chdir(current_dir)
+        return files_info
+
+    @staticmethod
+    def _get_changes_between_two_tags(tag_a: Tag, tag_b: Tag) -> List[GitFileMetadata]:
+        """
+        This method fetches all changes (modification & creation) between two tags,
+        and returns a list of changes containing:
+        - the file path
+        - the associated tag name
+        - the associated blob hash
+        """
+        files: List[GitFileMetadata] = []
+
+        commit_a = tag_a.commit
+        commit_b = tag_b.commit
+        commit_diff: DiffIndex = commit_a.diff(commit_b)
+
+        for diff in commit_diff:
+            diff: Diff = diff
+
+            if diff.a_blob and S_ISREG(diff.a_blob.mode):
+                files.append((diff.a_blob.path, tag_b.name, diff.a_blob.hexsha))
+            elif diff.b_blob and S_ISREG(diff.b_blob.mode):
+                files.append((diff.b_blob.path, tag_b.name, diff.b_blob.hexsha))
+        return files
+
+    def _get_diff_files(self, tags: List[Tag]) -> List[GitFileMetadata]:
+        """
+        This method retrieves all changes between a list of tags and returns them.
+        """
+        logger.info("Retrieving diff of all tags ...")
+
+        files: List[GitFileMetadata] = []
+
+        # This line makes couples with the n + 1 element. Example: (A,B), (B,C), ...
+        for (tag_a, tag_b) in zip(tags[:-1], tags[1:]):
+            files += self._get_changes_between_two_tags(tag_a, tag_b)
+        return files
+
+    def _get_tag_files(self, tag: Tag) -> List[GitFileMetadata]:
+        """
+        This method retrieves all files with their tag name and their blob hash in a tag.
+        """
+        files: List[GitFileMetadata] = []
+
+        for (file_path, blob_hash) in self.get_all_files_from_commit(tag.commit):
+            files.append((file_path, tag.name, blob_hash))
+        return files
+
+    @staticmethod
+    def _get_diff_versions(first_version: str, last_version: str, tags: List[Tag]) -> List[str]:
+        """
+        This method retrieves all tags between two tags.
+        """
+        tag_names = list(map(lambda tag: tag.name, tags))
+        return tag_names[tag_names.index(first_version):tag_names.index(last_version)]
+
+    def _save_hashes(
+        self,
+        session_scope,
+        files_info: List[FileMetadata],
+        tags: List[Tag],
+        technology: str
+    ):
+        """
+        This method saves all files with their hash & theirs versions to the database.
+        """
+        with session_scope() as session:
+            file_record = {}
+
+            self._database.insert_versions(session, technology, tags)
+            for (file_path, tag_name, file_hash) in files_info:
+                (last_version, last_hash) = file_record.get(file_path) or (None, None)
+
+                self._database.insert_file(session, technology, file_path)
+
+                if last_version is not None:
+                    # We retrieve all the versions between the last version of the file and this one
+                    # and then we add them to the last hash
+                    versions = self._get_diff_versions(last_version, tag_name, tags)
+                    self._database.insert_or_update_hash(session, last_hash, technology, versions)
+
+                self._database.insert_or_update_hash(session, file_hash, technology, [tag_name])
+                file_record[file_path] = (tag_name, file_hash)
+
+    def clone_checkout_and_compute_hashs(self, session_scope, url: str):
+        """
+        This method clones the repository from url, retrieves tags, compares each tags to retrieve only modified files,
+        computes their hashes and then stores the tags & files information in the database.
         """
         technology = url.split('.git')[0].split('/')[-1]
-        with tempfile.TemporaryDirectory() as tmp_dir_name:
+        tags: List[Tag] = []
+        files: List[GitFileMetadata] = []
 
+        with tempfile.TemporaryDirectory() as tmp_dir_name:
             try:
-                repository = self.clone_repository(technology, url, tmp_dir_name)
+                repo = self.clone_repository(url, tmp_dir_name)
             except GitCommandError as error:
                 logger.warning(f"Error while cloning repository on {url}: {error}")
                 return
 
-            logger.debug("Retrieving tags ...")
-            path = f"{tmp_dir_name}/{technology}"
-            git_tags = self.get_tags(repository)
-            logger.debug(f"Git tags : {git_tags}")
+            logger.info("Retrieving tags ...")
+            tags = repo.tags.copy()
 
-            logger.debug(f"Inserting tags for {technology} ...")
-            with session_scope() as session:
-                self._database.insert_versions(session, technology, git_tags)
+            logger.info("Retrieving files from the first tag ...")
+            files += self._get_tag_files(tags[0])
 
-            logger.debug(f"Retrieving tags from database for {technology}")
-            with session_scope() as session:
-                tags = self._database.get_versions(session, technology)
-                logger.debug(f"Database tags : {tags}")
+            logger.info("Retrieving only modified files between the tags ...")
+            files += self._get_diff_files(tags)
 
-                for tag in tags:
-                    logger.debug(f"Checkout and compute hashs for tag {tag} ...")
-                    self.checkout_and_compute(
-                        session, path, repository, tag.version)
+            logger.info("Generating hashes ...")
+            files_info = self._hash_files(files, tmp_dir_name)
+
+        logger.info("Saving hashes ...")
+        self._save_hashes(session_scope, files_info, tags, technology)
