22d5631b6f577f32ce2e2a40dc4ca87f42ef9525
Oussama Benghechoua <benghechoua.oussama@gmail.com>
2023-06-09 16:30:51 +0200

    Fix the problem of versions for unmodified files
    The problem was for the unmodified files between different tags, the database contain only the first tag

--- hashtheplanet/config/extensions_list.py
+++ hashtheplanet/config/extensions_list.py
@@ -1,4 +1,4 @@
 """
 This file contains a Regular Expression of files to exclude, allowing HashThePlanet to take only interesting files
 """
-EXCLUDED_FILE_PATTERN = '(test.|.*\\.(php|jsp|asp|aspx|db|yml|sh|sql|pl)$)'
+EXCLUDED_FILE_PATTERN = '(test.|.*\\.(php|jsp|asp|aspx|db|yml|sh|sql|pl|mysql|inc|conf|class|module)$)'
